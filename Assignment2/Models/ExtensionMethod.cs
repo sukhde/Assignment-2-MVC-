712ecd5a69eb81cff58f191c8fcb3bcb81547d76
SD18 <SD18@mitt.ca>
2018-10-26 11:29:31 -0500

    cc

---
+++ Assignment2/Models/ExtensionMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment2.Models
+{
+    public static class ExtensionMethod
+    {
+      public static int ToAge()
+        {
+            return ();
+        }
+    }
+}
